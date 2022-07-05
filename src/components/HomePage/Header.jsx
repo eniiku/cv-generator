12526c1b7c6b1021aa8ed21e14285886828f522c
tekyuma <lawrencedaviid@gmail.com>
2022-07-05 20:09:42 +0100

    fix tags in header

---
+++ src/components/HomePage/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <div className="header-logo">
+        <strong>tek</strong>
+        <strong>company</strong>
+      </div>
+      <div className="header-links">
+        <a href="linkedin.com">icon</a>
+        <a href="twitter.com">icon</a>
+        <a href="github.com">icon</a>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
