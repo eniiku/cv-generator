711956861be549d3bd26bc19804bbab6f2a746ad
tekyuma <lawrencedaviid@gmail.com>
2022-07-05 19:54:42 +0100

    parse home page layout

---
+++ src/components/Home/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div>
+      created by David Enikuomehin <a href="github.com/tekyuma">icon</a>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/components/Home/Hero.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Hero = () => {
+  return (
+    <main>
+      <div className="hero-text">
+        <h1 className="hero-text-heading">the best online cv generator</h1>
+        <p className="hero-text-subheading">
+          Leave it to us to build you a stella resume, so you can land that
+          dream job!🚀
+        </p>
+      </div>
+      <div className="hero-img">some image lol</div>
+      <div className="feedback">feedback</div>
+    </main>
+  );
+};
+
+export default Hero;
